a33c1db44b36b50447746abc6094f2ff3ab0a7aa
wanglifeng <wanglifeng@yidian-inc.com>
2016-07-09 23:18:03 +0800

    use sshpass for ssh connection

---
+++ sshpass.go
@@ -0,0 +1,10 @@
+package main
+//#include "sshpass.h"
+import "C"
+
+func sshpass(host, password string) {
+	host_c := C.CString(host)
+	password_c := C.CString(password)
+
+	C.sshpass(host_c, password_c)
+}
